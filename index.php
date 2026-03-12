52be0b5e7c112ffe40b8690c58f043e656270923
Anhdaoduy <anh.dao-duy@orange.fr>
2026-03-12 18:33:09 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>CI/CD</title>
+    <style>
+        body { 
+            background-color: grey;
+            font-family: sans-serif;
+            display: flex; justify-content: center; align-items: center; height: 100vh;
+        }
+        .card { background: white; padding: 20px; border-radius: 10px; box-shadow: 0 4px 6px rgba(0,0,0,0.1); }
+    </style>
+</head>
+<body>
+    <div class="card">
+        <h1>Version : 1.0.0</h1>
+        <p>Statut : Déploiement Automatique Réussi !</p>
+    </div>
+</body>
+</html>  
